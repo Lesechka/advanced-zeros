c46e1733ee1ae16c66ed5d7ff5da7ae969f46f03
Lesechka <littlecare@mail.ru>
2018-10-03 11:01:09 +0300

    Solution2

--- src/index.js
+++ src/index.js
@@ -2,35 +2,40 @@ module.exports = function getZerosCount(number, base) {
   let zeros = 0;
 
 
-   function simpleMultipliers(x) {
-       let j = 0;
-       let i = 2;
-       let arr = [];
-       do {
-           if (x % i === 0) {
-               arr[j] = i;
-               j++;
-               x = x / i;
-           }
-           else {
-               i++;
-           }
-       }
-       while (i < x) ;
-       arr[j] = i;
-       return arr;
-   }
-           /*function arrayMax(arr) {
-               return arr.reduce(function (p, v) {
-                   return (p < v ? p : v);
-               });
-*/
-               let maxMultiplier = Math.max(...(simpleMultipliers(base)));
+  function simpleMultipliers(x) {
+      let j = 0;
+      let i = 2;
+      let arr = [];
+      do {
+        if (x === 2) {
+          arr[j] = x;
+        }
+      else if ( x % i === 0) {
+              arr[j] = i;
+              j++;
+              x = x / i;
+          }
+          else {
+              i++;
+          }
+      }
+      while (i < x);
+      arr[j] = i;
+      return arr;
+  }
+  let baseArray = simpleMultipliers(base);
+let maxMultiplier = Math.max(...baseArray);
+  let countMaxMultiplier = 0;
+  for(let j = 0; j < baseArray.length; ++j){
+      if(baseArray[j] === maxMultiplier)
+          countMaxMultiplier++;
+  }
 
-
-               while (number > 0) {
-                   number = Math.floor(number / maxMultiplier);
-                   zeros += number;
-               }
-               return zeros;
+console.log(maxMultiplier);
+console.log(countMaxMultiplier);
+  while (number > 0){
+      number = Math.floor(number/maxMultiplier);
+      zeros += number;
+  }
+  return Math.floor(zeros/countMaxMultiplier);
 }
